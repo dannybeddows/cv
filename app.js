3410ceffa93be1258e4a2f27d81dc28ab80a4c3c
Dani Beddo <danbeddows@gmail.com>
2021-05-04 14:55:35 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+    
+    burger.classList.toggle('toggle');
+    
+    });
+} 
+
+navSlide(); 
