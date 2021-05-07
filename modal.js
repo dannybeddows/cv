1f79ad8c780aaa5d44b0fe42e8ba0f4fd8aa2268
Dani Beddo <danbeddows@gmail.com>
2021-05-07 15:04:27 +0700

    Update modal.js

--- modal.js
+++ modal.js
@@ -2,11 +2,12 @@ const open = document.getElementsByClassName('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
-open.addEventListener('click', () => {
+for (var i=0; i < open.length; i++) {
+    open[i].addEventListener('click', function() {
     modal_container.classList.add('show');
 });
 
 
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
-});
+})};
